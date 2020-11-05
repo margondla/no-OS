e0a82178f5fbaa8119a01d0cc173229c97dce398
Dragos Bogdan <dragos.bogdan@analog.com>
2020-11-05 13:51:17 +0200

    projects: ad9361: src.mk: Add linux platform support
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- projects/ad9361/src.mk
+++ projects/ad9361/src.mk
@@ -20,12 +20,19 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c			\
 	$(DRIVERS)/spi/spi.c						\
 	$(DRIVERS)/gpio/gpio.c						\
 	$(NO-OS)/util/util.c
-SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c					\
-	$(PLATFORM_DRIVERS)/delay.c
+SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c
+ifeq (linux,$(strip $(PLATFORM)))
+SRCS +=	$(PLATFORM_DRIVERS)/linux_delay.c
+SRCS +=	$(PLATFORM_DRIVERS)/linux_spi.c
+SRCS +=	$(PLATFORM_DRIVERS)/linux_gpio.c
+else
+SRCS +=	$(PLATFORM_DRIVERS)/delay.c
+endif
 ifeq (xilinx,$(strip $(PLATFORM)))
 SRCS +=	$(PLATFORM_DRIVERS)/xilinx_spi.c				\
 	$(PLATFORM_DRIVERS)/xilinx_gpio.c
-else
+endif
+ifeq (altera,$(strip $(PLATFORM)))
 SRCS +=	$(PLATFORM_DRIVERS)/altera_spi.c				\
 	$(PLATFORM_DRIVERS)/altera_gpio.c
 endif
@@ -49,8 +56,13 @@ INCS += $(DRIVERS)/rf-transceiver/ad9361/ad9361.h			\
 INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h			\
 	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.h			\
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h
+ifeq (linux,$(strip $(PLATFORM)))
+INCS +=	$(PLATFORM_DRIVERS)/linux_spi.h					\
+	$(PLATFORM_DRIVERS)/linux_gpio.h
+else
 INCS +=	$(PLATFORM_DRIVERS)/spi_extra.h					\
 	$(PLATFORM_DRIVERS)/gpio_extra.h
+endif
 INCS +=	$(INCLUDE)/axi_io.h						\
 	$(INCLUDE)/spi.h						\
 	$(INCLUDE)/gpio.h						\
